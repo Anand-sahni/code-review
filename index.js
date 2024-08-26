f06b8622005ef61ba4bf61c8a92bcf44ccf40af0
Anand Sahni <75078213+Anand-sahni@users.noreply.github.com>
2024-08-26 09:18:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // program to generate fibonacci series up to n terms
 
+// new comment line
+
 // take input from the user
 const number = parseInt(prompt('Enter the number of terms: '));
 let n1 = 0, n2 = 1, nextTerm;
